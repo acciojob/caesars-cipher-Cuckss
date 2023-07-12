19ccf5a635c797467b7efc68e7858661d82ded02
Cuckss <122050270+Cuckss@users.noreply.github.com>
2023-07-12 16:58:01 +0530

    created a submission

--- script.js
+++ script.js
@@ -9,23 +9,18 @@ const lookup = {
   'U': 'H','V': 'I','W': 'J','X': 'K',
   'Y': 'L','Z': 'M', '?': '?', ',': ','
 };
-
-function rot13(encodedStr){
-   let decodedArr = []; // Your Result goes here
-  // Only change code below this line
-    
-      for (let i = 0; i < encodedStr.length; i++) {
-    let char = encodedStr[i];
-    let decodedChar = lookup[char] || char; // Use the lookup table, or keep the character unchanged
-
-    decodedArr.push(decodedChar);
+function rot13(str){
+  var solved="";
+  for(var i=0;i<str.length;i++){
+    var ascii=str[i].charCodeAt();
+    if(ascii>=65 && ascii<=77){
+      solved+=String.fromCharCode(ascii+13);
+    }else if(ascii>=78 && ascii<=90){
+      solved+=String.fromCharCode(ascii-13);
+    }
   }
-
-  return decodedArr.join('');
+  return solved;
 }
-
-// You can test your code by running the above function and printing it to console by pressing the run button at the top. To run it with input 36, uncomment the following line
-
-// console.log(rot13("SERR YBIR? NPPVBWBO"));
+console.log(rot13("AKSHAT"))
 
 module.exports = rot13;
